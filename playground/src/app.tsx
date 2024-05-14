14da54cac5bed2dcf733f2d84e5e8c0964db1c54
Vitalij Ryndin <crashmax.dev@gmail.com>
2024-05-14 21:06:45 +0800

    chore: button example

--- playground/src/app.tsx
+++ playground/src/app.tsx
@@ -4,6 +4,9 @@ export function App() {
   return (
     <div class="flex flex-col gap-4 p-4">
       <Button size="small" variant="secondary" class="underline">Small</Button>
+      <Button>Destructive</Button>
+      <Button variant="secondary">Destructive</Button>
+      <Button variant="success">Destructive</Button>
       <Button variant="destructive">Destructive</Button>
     </div>
   )
