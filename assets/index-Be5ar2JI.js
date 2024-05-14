dedaaa53ee7062bbb9dc80e2fd80c05df5086799
crashmax-dev <crashmax.dev@gmail.com>
2024-05-14 11:08:28 +0000

    Deploying to gh-pages from @ crashmax-dev/tailwind-plugin@6ddc2750145322b509e83d1fa1fcbc1e9537505b 🚀

---
+++ assets/index-Be5ar2JI.js
@@ -0,0 +1,1 @@
+import{M,u as a}from"./index-BJ63lO9b.js";import"./index-Cu97k2vy.js";import"./iframe-C6TOUNYK.js";import"../sb-preview/runtime.js";import"./index-CcisS4Rv.js";import"./index-DXimoRZY.js";import"./index-DrFu-skq.js";export{M as MDXProvider,a as useMDXComponents};
